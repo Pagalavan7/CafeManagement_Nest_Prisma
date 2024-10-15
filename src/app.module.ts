93ac984731ed4e047a0168d80ba202ba0666b5f2
Pagalavan7 <pagalavanvetri@gmail.com>
2024-10-15 09:51:38 +0530

    user-details initial file generate

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
+import { UserDetailsModule } from './user-details/user-details.module';
+import { UserDetailsModule } from './user-details/user-details.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserDetailsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
